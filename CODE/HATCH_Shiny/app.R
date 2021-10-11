ca4847ea19360fd4137aaba155ae50e45a5e91cc
Tim Harrold <timjharrold@gmail.com>
2021-10-11 12:46:44 -0400

    Node labels
    - Working nodes into data frame, adding  labels / mutating data

--- CODE/HATCH_Shiny/app.R
+++ CODE/HATCH_Shiny/app.R
@@ -9,12 +9,22 @@
 
 library(shiny)
 library(ggplot2)
+library(tidyverse)
 library(dplyr)
+library(visNetwork)
 source("directed_graph.R")
 
 
 # Load data
 agData <- read.csv("../../DATA_INPUTS/Spatial_data_inputs/Afghanistan_ImportsGlobalConstrained_2019.csv")
+
+# Define graph nodes
+nodes <- data.frame(
+  id=agData$X,
+  label = toString(agData$X),
+  group=nchar(agData$FAO_CropName))
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -31,33 +41,50 @@ ui <- fluidPage(
                         max = 50,
                         value = 30)
         ),
-
+        sidebarPanel(
+          fluidRow(
+            column(12,
+                   dataTableOutput('table')
+            )
+           )
+        )
+    ),
         #Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("dGraph")
         )
-    )
+    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-  
-
-    agData
-
+    
   
     output$distPlot <- renderPlot({
       
+
       ggplot(data = agData,
-             mapping=aes(x=Country, y=FAO_CropName))+ geom_jitter()
+             mapping=aes(x=FAO_CropName, y=Calories))+ geom_area()
       #x <- as.numeric(data$Potassium)
       #bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
       #hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
+   
 
-
+    output$dGraph <- renderPlot({
+      
+      visNetwork(nodes, height = "500px", width = "100%", main="Bipartite Graph") %>%
+        visPhysics(solver = "forceAtlas2Based", 
+                   forceAtlas2Based = list(gravitationalConstant = -500))
+      
+    })
+    
+    
+    output$table <- renderDataTable(agData)
+    
+    
 }
 
 # Run the application 
